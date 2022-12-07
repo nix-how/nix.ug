8b29595d431ec027057dbba989800fbf55261c70
matthewcroughan <matt@croughan.sh>
2022-12-07 14:25:19 +0000

    nugs: add berlin/zurch NUGs

---
+++ lists/NUGs/berlin/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "online offline";
+  name = "Berlin NixOS Meetup";
+  subtitle =  "Thursdays Online, Tuesdays Offline";
+  tag = "Germany";
+  target = "_blank";
+  url = "https://www.meetup.com/Berlin-NixOS-Meetup/";
+}
---
+++ lists/NUGs/zurich/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person";
+  name = "NixOS User Group Zürich";
+  subtitle =  "No strict schedule";
+  tag = "Switzerland";
+  target = "_blank";
+  url = "https://www.meetup.com/NixOS-User-Group-Zurich/";
+}
