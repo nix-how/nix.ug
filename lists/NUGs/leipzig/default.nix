b94165749ac7493478686c17be23f001cb814030
tuxa <69870860+mrtuxa@users.noreply.github.com>
2023-01-30 13:14:43 +0100

    Create default.nix

---
+++ lists/NUGs/leipzig/default.nix
@@ -0,0 +1,8 @@
+{
+keywords = "First Meetup"
+name = "leipzig.nix | Nix User Group"
+subtitle = "Was machen wir heute? Nix!"
+tag = "Germany";
+target = "_blank";
+url = "https://leipzig-nix.github.io//";
+}
