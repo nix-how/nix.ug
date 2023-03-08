5cda0ca0927dcdbc710fbbe2cc905382a90481be
Matthias Thym <git@thym.at>
2023-03-08 11:27:39 +0100

    NUGs: add NixOS Austria

---
+++ lists/NUGs/austria/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline online in-person austria österreich graz vienna";
+  name = "NixOS User Group Austria";
+  subtitle = "No strict schedule";
+  tag = "Austria";
+  target = "_blank";
+  url = "https://nixos.at";
+}
