5a26175c99aaf3012e50e93e8acba44b473ea2dc
Janik H <janik@aq0.de>
2023-06-04 20:02:29 +0200

    NUGs: add Nürnberg

---
+++ lists/NUGs/nürnberg/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person nürnberg germany";
+  name = "Nürnberg NixOS Meetup";
+  subtitle =  "Every third Tuesday at Nerdberg E.v.";
+  tag = "Germany";
+  target = "_blank";
+  url = "https://matrix.to/#/#nixos-nuernberg:nerdberg.de";
+}
