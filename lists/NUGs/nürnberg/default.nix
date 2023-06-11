83f760895f4989bf08fe16b493ab2a2ded1362b6
Janik H <janik@aq0.de>
2023-06-11 11:42:45 +0200

    fix spelling and link of nürnberg ug

--- lists/NUGs/nürnberg/default.nix
+++ lists/NUGs/nürnberg/default.nix
@@ -1,8 +1,8 @@
 {
   keywords = "offline in-person nürnberg germany";
   name = "Nürnberg NixOS Meetup";
-  subtitle =  "Every third Tuesday at Nerdberg E.v.";
+  subtitle =  "Every third Tuesday at Nerdberg e.V.";
   tag = "Germany";
   target = "_blank";
-  url = "https://matrix.to/#/#nixos-nuernberg:nerdberg.de";
+  url = "https://matrix.to/#/#nixos-nuernberg:matrix.org";
 }
