dcd5abfa83669ba6754ef8ac4d7fe66274454ab2
matthewcroughan <matt@croughan.sh>
2023-08-31 19:08:14 +0100

    Delete lists/NUGs/leipzig

--- lists/NUGs/leipzig/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  keywords = "First Meetup";
-  name = "leipzig.nix | NixOS User Group";
-  subtitle = "Was machen wir heute? Nix!";
-  tag = "Germany";
-  target = "_blank";
-  url = "https://leipzig.nix.ug";
-}
