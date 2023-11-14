998faa432d25839487543b6ac91c5165e265e113
Johannes Gerold <git+commit@jgero.me>
2023-11-14 20:21:13 +0100

    feat: add Aalen meetup

---
+++ lists/NUGs/aalen/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person aalen germany";
+  name = "Aalen NixOS Meetup";
+  subtitle =  "Every first Tuesday";
+  tag = "Germany";
+  target = "_blank";
+  url = "https://matrix.to/#/#nixos-aalen:matrix.org";
+}
