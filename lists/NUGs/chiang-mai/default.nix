c5b8088355dec6e2bb2b6c896ce69789d13a5180
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-18 05:09:22 +0700

    NixOS Chiang Mai -> Nix Chiang Mai

--- lists/NUGs/chiang-mai/default.nix
+++ lists/NUGs/chiang-mai/default.nix
@@ -1,6 +1,6 @@
 {
   keywords = "thailand chiang mai chiangmai"; # space separated keywords
-  name = "NixOS Chiang Mai";
+  name = "Nix Chiang Mai";
   subtitle = "Regular Meetups in Chiang Mai";
   tag = "Thailand";
   target = "_blank"; # optional html <a> tag target attribute
