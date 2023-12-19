7b723e88228659baefd3f5a55bff5557420e6858
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-19 09:34:07 +0700

    thailand -> chiang-mai

--- lists/NUGs/chiang-mai/default.nix
+++ lists/NUGs/chiang-mai/default.nix
@@ -1,7 +1,7 @@
 {
   keywords = "thailand chiang mai chiangmai"; # space separated keywords
-  name = "NixOS Thailand";
-  subtitle = "Regular Meetups in Thailand";
+  name = "NixOS Chiang Mai";
+  subtitle = "Regular Meetups in Chiang Mai";
   tag = "Thailand";
   target = "_blank"; # optional html <a> tag target attribute
   url = "https://matrix.to/#/#nixos-thailand:matrix.org";
