1765b3f94d4b4942ce579455a21958e5737399e1
matthewcroughan <matt@croughan.sh>
2024-01-24 12:53:37 +0000

    modify description of Events/nix.camp

--- lists/Events/nix.camp/default.nix
+++ lists/Events/nix.camp/default.nix
@@ -1,7 +1,7 @@
 {
   keywords = "";
   name = "Nix.Camp";
-  subtitle = "Yearly, often in Wales";
+  subtitle = "Yearly, often in UK";
   tag = "United Kingdom/Wales";
   target = "_blank";
   url = "https://nix.camp/";
