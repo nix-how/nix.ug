ffa159da8cb8a5a5c15995a626f60dbd4384cf67
unrooted <konrad.root.klawikowski@gmail.com>
2024-01-31 04:31:06 +0100

    add NUG Poland

---
+++ lists/NUGs/poland/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "poland offline in-person warsaw";
+  name = "Nix User Group Poland";
+  subtitle =  "No strict schedule";
+  tag = "Poland";
+  target = "_blank";
+  url = "https://discord.gg/eJnKKjQ3Bc";
+}
