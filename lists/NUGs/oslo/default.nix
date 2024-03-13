c10307d5ca3a7f934d053e7b5357035972aa7c71
Stig <stig@stig.io>
2024-03-13 02:39:13 +0100

    Oslo: update subtitle

--- lists/NUGs/oslo/default.nix
+++ lists/NUGs/oslo/default.nix
@@ -2,7 +2,7 @@
   keywords = "online offline"; # space separated keywords
   name = "Oslo NixOS User Group";
   tag = "Oslo";
-  subtitle = "Whenever Stig feels like it";
+  subtitle = "No strict schedule";
   target = "_blank"; # optional html <a> tag target attribute
   url = "https://www.meetup.com/Oslo-NixOS-User-Group/";
 }
