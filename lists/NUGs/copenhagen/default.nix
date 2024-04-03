58a296bff608a274040e71403a64e87e72693335
estka <estridkark@gmail.com>
2024-04-03 20:21:25 +0200

    update naming to align across platforms

--- lists/NUGs/copenhagen/default.nix
+++ lists/NUGs/copenhagen/default.nix
@@ -1,6 +1,6 @@
 {
-  keywords = "København Roskilde Malmö Lund Sjælland Skåne";
-  name = "Greater Copenhagen NixOS User Group";
+  keywords = "Copenhagen København Roskilde Malmö Lund Sjælland Skåne";
+  name = "NixOS Copenhagen User Group";
   subtitle =  "4 times a year";
   tag = "Denmark";
   target = "_blank";
