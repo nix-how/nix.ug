6bfb97a6948ff0c7e6cce10461d4d55fa597657e
Tom Bereknyei <tomberek@gmail.com>
2024-04-03 21:15:17 -0400

    init: Maryland NUG

---
+++ lists/NUGs/maryland/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person maryland dc virginia meetup";
+  name = "Maryland Nix Users Group";
+  subtitle = "No strict schedule";
+  tag = "America";
+  target = "_blank";
+  url = "https://discourse.nixos.org/t/april-23-maryland-dc-virginia-nix-users-group/42723";
+}
