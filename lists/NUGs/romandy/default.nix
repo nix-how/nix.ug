dfddc660f3aaab20dc52414b4b7cd3a461b6fbaa
zimbatm <zimbatm@zimbatm.com>
2024-06-18 15:36:29 +0200

    add the Nix Romandy User Group

---
+++ lists/NUGs/romandy/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person";
+  name = "Nix Romandy";
+  subtitle =  "Swiss French Nix/NixOS community";
+  tag = "Switzerland";
+  target = "_blank";
+  url = "https://www.meetup.com/nix-romandy/";
+}
