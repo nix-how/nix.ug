2c10026dfcf1046c760fde983b9b11a4213c6aaa
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-08-16 14:56:31 +0200

    nug/milan: add

---
+++ lists/NUGs/milan/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person milan italy";
+  name = "Nix Milano";
+  subtitle =  "The Milan Nix community";
+  tag = "Italy";
+  target = "_blank";
+  url = "https://milano.nix.pizza";
+}
