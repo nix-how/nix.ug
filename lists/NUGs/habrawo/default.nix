cd70cb7fd86a97e54e7f72a8e989aae624f5df5e
Jacek Galowicz <jacek@galowicz.de>
2024-09-13 18:44:35 +0200

    Add NUG HaBraWo

---
+++ lists/NUGs/habrawo/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "online offline";
+  name = "Hannober Braunschweig Wolfsburg Nix User Group";
+  subtitle = "Monthly meetups in H/BS/WOB";
+  tag = "Germany";
+  target = "_blank";
+  url = "https://habrawo.nix.ug";
+}
