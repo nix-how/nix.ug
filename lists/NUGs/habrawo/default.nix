84c169c8d46fc3d6af9234288be5199f2a7b994a
Jacek Galowicz <jacek@galowicz.de>
2024-09-13 19:21:42 +0200

    Fix typo, switch from png to svg

--- lists/NUGs/habrawo/default.nix
+++ lists/NUGs/habrawo/default.nix
@@ -1,6 +1,6 @@
 {
   keywords = "online offline";
-  name = "Hannober Braunschweig Wolfsburg Nix User Group";
+  name = "Hannover Braunschweig Wolfsburg Nix User Group";
   subtitle = "Monthly meetups in H/BS/WOB";
   tag = "Germany";
   target = "_blank";
