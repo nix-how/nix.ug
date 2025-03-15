34488df50899280ebd1562bc4d887a6218eb020b
Markus Kowalewski <markus.kowalewski@gmail.com>
2025-03-15 16:00:16 +0100

    lists: add Stockholm NixOS meetup group

---
+++ lists/NUGs/stockholm/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "off-line in-person meetings";
+  name = "Stockholm NixOS Meetup Group";
+  subtitle =  "monthly meetings";
+  tag = "Sweden";
+  target = "_blank";
+  url = "https://matrix.to/#/#stockholm:nixos.org";
+}
