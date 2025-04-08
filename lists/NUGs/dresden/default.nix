26ba17d52d3db980a4cdd473fb80756837a9c710
Sandro <sandro.jaeckel@gmail.com>
2025-04-09 00:24:59 +0200

    Update Dresden homepage and name

--- lists/NUGs/dresden/default.nix
+++ lists/NUGs/dresden/default.nix
@@ -1,8 +1,8 @@
 {
   keywords = "Meetup";
-  name = "nix-meetup | Dresden";
+  name = "nix-meetup Dresden";
   subtitle = "about once a month";
   tag = "Germany";
   target = "_blank";
-  url = "https://c3d2.de/nix-meetup.html";
+  url = "https://nix-meetup.c3d2.de/about";
 }
