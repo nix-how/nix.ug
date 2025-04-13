1c100894c31b98e7558004f175dbedf738350e42
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2025-04-13 16:35:59 +0200

    Add Nix Bern

---
+++ lists/NUGs/bern/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person bern switzerland";
+  name = "Nix Bern";
+  subtitle = "The Bern Nix community";
+  tag = "Switzerland";
+  target = "_blank";
+  url = "https://www.meetup.com/nix-bern/";
+}
