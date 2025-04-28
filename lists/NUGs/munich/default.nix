40d2d9245b51c9c00dc7f38522eb7c99b1816094
WilliButz <willibutz@posteo.de>
2025-04-28 11:24:02 +0200

    add meetup in Munich/Germany

---
+++ lists/NUGs/munich/default.nix
@@ -0,0 +1,13 @@
+{
+  keywords = "offline in-person munich germany";
+  name = "Munich NixOS Meetup";
+  subtitle = ''
+    Agendaless meetup about NixOS, nixpkgs, Nix and the rest of the Nix ecosystem.
+    On the second Friday of each month.
+    Check the calendar in µCCC wiki or the Matrix channel https://matrix.to/#/#muenchen:nixos.org
+    for confirmation.
+  '';
+  tag = "Germany";
+  target = "_blank";
+  url = "https://wiki.muc.ccc.de/nixos";
+}
