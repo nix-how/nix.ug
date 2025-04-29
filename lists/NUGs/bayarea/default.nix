4d745d97a40e29ab170b333fe08293ac016e09ab
Salar Rahmanian <code@softinio.com>
2025-04-28 19:32:51 -0700

    Use registered domain instead of luma directly

--- lists/NUGs/bayarea/default.nix
+++ lists/NUGs/bayarea/default.nix
@@ -1,8 +1,8 @@
 {
   keywords = "self hosted reddit";
   name = "Bay Area Nix/NixOS User Group";
-  subtitle =  "No strict schedule";
+  subtitle = "No strict schedule";
   tag = "America";
   target = "_blank";
-  url = "https://lu.ma/nixos";
+  url = "https://bayareanixos.com";
 }
