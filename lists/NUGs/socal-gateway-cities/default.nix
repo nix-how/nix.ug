ff6dff434dff1a3628f6d12c09a999d9bd871c66
Robert James Hernandez <rob@sarcasticadmin.com>
2025-05-04 00:56:35 +0000

    nugs.socal-gateway-cities: init

---
+++ lists/NUGs/socal-gateway-cities/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person california";
+  name = "Socal Gateway Cities Nix Users Group";
+  subtitle = "No strict schedule";
+  tag = "America";
+  target = "_blank";
+  url = "https://gatewaycities.socal-nug.com/";
+}
