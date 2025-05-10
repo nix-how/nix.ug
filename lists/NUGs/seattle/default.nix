84b16b2ba87ee51c1dfd6c67aff70f5f86b1796e
eureka-cpu <github.eureka@gmail.com>
2025-05-10 13:29:49 -0700

    Add Seattle Nix User Group

---
+++ lists/NUGs/seattle/default.nix
@@ -0,0 +1,11 @@
+{
+  keywords = "offline in-person seattle washington";
+  name = "Seattle Nix User Group";
+  subtitle = ''
+    In-person beginner friendly meet ups and online chat group ❄️
+    Check the #Schedule chat in Matrix for details!
+  '';
+  tag = "Seattle";
+  target = "_blank";
+  url = "https://matrix.to/#/#seattle-nix-user-group:matrix.org";
+}
