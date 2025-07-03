ec8edff14767d032208df1c8156242312043005d
Arnout Engelen <arnout@bzzt.net>
2025-07-03 11:31:12 +0200

    Link to NixNL page, add logo

---
+++ lists/NUGs/netherlands/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = ""; # space separated keywords
+  name = "NixNL";
+  subtitle =  "Dutch Nix Community";
+  tag = "Netherlands";
+  target = "_blank"; # optional html <a> tag target attribute
+  url = "https://nl.meet.nix-community.org/";
+}
