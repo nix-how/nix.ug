8ba0ef784f0ad09974064f7d923944bbdc414a2b
Adrian Block <adrian@adrblo.de>
2025-08-06 19:54:06 +0200

    added Nix UG Paderborn

---
+++ lists/NUGs/paderborn/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "online offline germany nrw";
+  name = "NixDorf Paderborn";
+  subtitle = "4 times a year";
+  tag = "Germany";
+  target = "_blank";
+  url = "https://nix-paderborn.github.io/";
+}
