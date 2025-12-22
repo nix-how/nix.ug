9b463d4a4e56def443f29139de0b48d4e15b307f
Lin Jian <me@linj.tech>
2025-12-22 20:51:22 +0800

    Add Nix CN community

---
+++ lists/NUGs/cn/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "online offline in-person cn china chinese meetup";
+  name = "Nix CN";
+  subtitle = "The Chinese Nix community";
+  tag = "China";
+  target = "_blank";
+  url = "https://nixos-cn.org/";
+}
