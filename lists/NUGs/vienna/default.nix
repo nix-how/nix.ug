5705ea859d290413f164482f74a3de4824551535
phaer <hello@phaer.org>
2026-02-03 19:56:18 +0100

    add NixOS Vienna user group

---
+++ lists/NUGs/vienna/default.nix
@@ -0,0 +1,8 @@
+{
+  keywords = "offline in-person austria österreich vienna";
+  name = "NixOS User Group Vienna";
+  subtitle = "4 times a year: February, May, August, November";
+  tag = "Austria";
+  target = "_blank";
+  url = "https://vienna.nix.ug";
+}
