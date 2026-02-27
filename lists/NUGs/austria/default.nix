b21a4f1bc942f32a1f2e79eed41933d86d6bb0cd
phaer <phaer@phaer.org>
2026-02-27 14:33:42 +0100

    Retire NixOS User Group Austria...
    Vienna's NUG has its own website now, and there are no active user groups
    outside Vienna atm. Besides meetups in Vienna, there has only ever been
    a single online meeting 3 years ago (March 2023).
    
    Should that situation change, we can quickly restore it. But as Vienna's
    new website (vienna.nix.ug) isn't linked on nixos.at yet due to
    communication issues, listing both could lead to confusion and I would
    therefore propose to remove it for the time being, unless there are
    objections.

--- lists/NUGs/austria/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  keywords = "offline online in-person austria österreich graz vienna";
-  name = "NixOS User Group Austria";
-  subtitle = "No strict schedule";
-  tag = "Austria";
-  target = "_blank";
-  url = "https://nixos.at";
-}
